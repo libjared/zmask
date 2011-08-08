15216bd9aaabe09b81925ed1a25179e47540ac7e
Jonas Eriksson <zqad@acc.umu.se>
2011-08-08 13:05:15 +0200

    Q0: Add option of running flip vert/horiz and XOR

--- src/org/zkt/zmask/masks/Q0.java
+++ src/org/zkt/zmask/masks/Q0.java
@@ -25,13 +25,16 @@ import org.zkt.zmask.utils.PropertyException;
 import org.zkt.zmask.utils.PropertyHandler;
 
 /**
- * The Q0 mask, basically horizontal+vertical glass and invert
+ * The Q0 mask, basically horizontal+vertical glass and invert with optional
+ * flip horizontal/vertical and XOR 0x80.
  *
  * @author zqad
  */
 public class Q0 implements Mask {
 	private boolean runHorizontalGlass, runVerticalGlass, runInvert;
+	private boolean runFlipHorizontal, runFlipVertical, runXOR;
 	private Mask horizontalGlass, verticalGlass, invert;
+	private Mask flipHorizontal, flipVertical, xor;
 	private Q0PropertyHandler propertyHandler;
 	private PropertyDescription[] propertyArray;
 
@@ -39,10 +42,16 @@ public class Q0 implements Mask {
 		horizontalGlass = new HorizontalGlass();
 		verticalGlass = new VerticalGlass();
 		invert = new Invert();
+		flipHorizontal = new FlipHorizontal();
+		flipVertical = new FlipVertical();
+		xor = new XOR();
 
 		runHorizontalGlass = true;
 		runVerticalGlass = true;
 		runInvert = true;
+		runFlipHorizontal = false;
+		runFlipVertical = false;
+		runXOR = false;
 
 		propertyHandler = new Q0PropertyHandler(this);
 
@@ -51,6 +60,9 @@ public class Q0 implements Mask {
 			new PropertyDescription("runVerticalGlass", PropertyDescription.TYPE_BOOLEAN, "Run vertical glass", propertyHandler),
 			new PropertyDescription("runHorizontalGlass", PropertyDescription.TYPE_BOOLEAN, "Run horizontal glass", propertyHandler),
 			new PropertyDescription("runInvert", PropertyDescription.TYPE_BOOLEAN, "Run invert", propertyHandler),
+			new PropertyDescription("runFlipVertical", PropertyDescription.TYPE_BOOLEAN, "Run vertical flip", propertyHandler),
+			new PropertyDescription("runFlipHorizontal", PropertyDescription.TYPE_BOOLEAN, "Run horizontal flip", propertyHandler),
+			new PropertyDescription("runXOR", PropertyDescription.TYPE_BOOLEAN, "Run XOR 0x80", propertyHandler),
 		};
 		this.propertyArray = propertyArray;
 	}
@@ -81,6 +93,15 @@ public class Q0 implements Mask {
 		if (runInvert)
 			image = invert.runMask(image);
 
+		if (runFlipHorizontal)
+			image = flipHorizontal.runMask(image);
+
+		if (runFlipVertical)
+			image = flipVertical.runMask(image);
+
+		if (runXOR)
+			image = xor.runMask(image);
+
 		return image;
 	}
 
@@ -106,6 +127,12 @@ public class Q0 implements Mask {
 				q0.runHorizontalGlass = ((Boolean)value).booleanValue();
 			else if (key.equals("runInvert"))
 				q0.runInvert = ((Boolean)value).booleanValue();
+			else if (key.equals("runFlipHorizontal"))
+				q0.runFlipHorizontal = ((Boolean)value).booleanValue();
+			else if (key.equals("runFlipVertical"))
+				q0.runFlipVertical = ((Boolean)value).booleanValue();
+			else if (key.equals("runXOR"))
+				q0.runXOR = ((Boolean)value).booleanValue();
 			else
 				throw new PropertyException(key);
 		}
@@ -117,6 +144,12 @@ public class Q0 implements Mask {
 				return new Boolean(q0.runHorizontalGlass);
 			else if (key.equals("runInvert"))
 				return new Boolean(q0.runInvert);
+			else if (key.equals("runFlipHorizontal"))
+				return new Boolean(q0.runFlipHorizontal);
+			else if (key.equals("runFlipVertical"))
+				return new Boolean(q0.runFlipVertical);
+			else if (key.equals("runXOR"))
+				return new Boolean(q0.runXOR);
 
 			throw new PropertyException(key);
 		}
